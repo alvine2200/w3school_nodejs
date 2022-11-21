1d3a357f3c624bdf6167b817862131b545c58e38
alvine2200 <alvinellavu@gmail.com>
2022-11-21 16:30:53 +0300

    connected node js with mysql

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const mysql = require("mysql2");
 const app = express();
 const port = 3000;
+const conn = mysql.createConnection({
+  host: "localhost",
+  user: "Alvine",
+  password: "Heidi2022",
+  database: "bodaboda",
+});
+
+conn.connect(function (err) {
+  if (err) throw err;
+  console.log("Mysql Connected");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World");
